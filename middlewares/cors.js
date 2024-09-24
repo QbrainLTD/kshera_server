290aedcc4da050e42cda89d00107f2f2bf937c53
QBrain LTD <140598320+QbrainLTD@users.noreply.github.com>
2024-09-24 14:12:11 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const corsMiddleware = cors({
   origin: [
     "http://127.0.0.1:5500",
-    "www.tzachCards.co.il",
     "http://localhost:5500",
     "http://localhost:3000",
     "http://localhost:5173",
