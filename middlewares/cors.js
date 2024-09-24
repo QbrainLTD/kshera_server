d53a55e7d51ed47d4249450114e422591fa8ad46
QBrain LTD <140598320+QbrainLTD@users.noreply.github.com>
2024-09-24 15:58:24 +0300

    newcors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,9 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   origin: [
-    "http://3.75.158.163",
-    "http://3.125.183.140",
-    "http://35.157.117.28",
+    "https://cardprojectidanarbelisite.onrender.com",
     "http://localhost:5173",
   ],
 });
