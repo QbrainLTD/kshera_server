9633f0cc406f56068211e510efcc526732e25d56
QBrain LTD <140598320+QbrainLTD@users.noreply.github.com>
2024-09-24 10:28:31 +0300

    first commit

---
+++ middlewares/cors.js
@@ -0,0 +1,13 @@
+const cors = require("cors");
+
+const corsMiddleware = cors({
+  origin: [
+    "http://127.0.0.1:5500",
+    "www.tzachCards.co.il",
+    "http://localhost:5500",
+    "http://localhost:3000",
+    "http://localhost:5173",
+  ],
+});
+
+module.exports = corsMiddleware;
